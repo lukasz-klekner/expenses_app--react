031c9fc9579cf1494b7d3e58d6957a9d127d949b
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-17 10:49:51 +0100

    creates ExpenseItem component and adds it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
-  return <div className='App'>React App</div>
+  return (
+    <div>
+      <ExpenseItem />
+    </div>
+  )
 }
 
 export default App
