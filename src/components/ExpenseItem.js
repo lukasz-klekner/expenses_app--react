3aac413193361141540cb239189cd273b1c36e42
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-18 13:03:19 +0100

    adds ExpenseDate components and uses it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,14 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
-const ExpenseItem = ({ expense }) => {
-  const month = expense.date.toLocaleString('en-US', { month: 'long' })
-  const day = expense.date.toLocaleString('en-US', { day: '2-digit' })
-  const year = expense.date.getFullYear()
-
-  return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <div className='expense-item__description'>
-        <h2>{expense.title}</h2>
-        <div className='expense-item__price'>${expense.amount}</div>
-      </div>
+const ExpenseItem = ({ expense }) => (
+  <div className='expense-item'>
+    <ExpenseDate date={expense.date} />
+    <div className='expense-item__description'>
+      <h2>{expense.title}</h2>
+      <div className='expense-item__price'>${expense.amount}</div>
     </div>
-  )
-}
+  </div>
+)
 
 export default ExpenseItem
