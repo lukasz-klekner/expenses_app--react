4792bdfaf8c16eb0d3c3f40eae10578e54534c42
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-18 18:58:29 +0100

    adds Card component and uses it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
+import Card from './Card'
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ expense }) => (
-  <div className='expense-item'>
+  <Card className='expense-item'>
     <ExpenseDate date={expense.date} />
     <div className='expense-item__description'>
       <h2>{expense.title}</h2>
       <div className='expense-item__price'>${expense.amount}</div>
     </div>
-  </div>
+  </Card>
 )
 
 export default ExpenseItem
