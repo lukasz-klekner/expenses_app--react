6b16944e0ef688b279bde05a6fa7f8578791620d
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-20 14:24:20 +0100

    adds ExpensesFilter and some styles & lifts the state up

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter'
 
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState('2022')
+
+  const filterChangeHandler = (selectedYear) => setFilteredYear(selectedYear)
+
   return (
-    <Card className='expenses'>
-      {expenses.map((expense) => (
-        <ExpenseItem key={expense.id} expense={expense} />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <Card className='expenses'>
+        {expenses.map((expense) => (
+          <ExpenseItem key={expense.id} expense={expense} />
+        ))}
+      </Card>
+    </div>
   )
 }
 
