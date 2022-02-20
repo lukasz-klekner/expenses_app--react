7e19b67b8772fcb74bf08019356438db74081f91
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-20 18:53:20 +0100

    adds toogleExpense and Cancel button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './ExpenseForm.css'
 
-const ExpenseForm = ({ onSaveExpenseData }) => {
+const ExpenseForm = ({ onSaveExpenseData, onCancel }) => {
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState('')
   const [date, setDate] = useState('')
@@ -56,6 +56,9 @@ const ExpenseForm = ({ onSaveExpenseData }) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = ({ onAddExpense }) => {
+  const [isFormShown, setIsFormShown] = useState(false)
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,11 +12,21 @@ const NewExpense = ({ onAddExpense }) => {
     }
 
     onAddExpense(expenseData)
+    toggleExpense()
   }
+
+  const toggleExpense = () => setIsFormShown((prevState) => !prevState)
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {isFormShown ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={toggleExpense}
+        />
+      ) : (
+        <button onClick={toggleExpense}>Add New Expense</button>
+      )}
     </div>
   )
 }
